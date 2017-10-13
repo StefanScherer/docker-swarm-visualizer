db6c6f3d7f5612b9784c91558181896e872ee5d2
Stefan Scherer <scherer_stefan@icloud.com>
2017-10-13 08:15:38 +0200

    Build Docker image in AppVeyor

--- build.ps1
+++ build.ps1
@@ -1,17 +1,6 @@
 $ErrorActionPreference = 'Stop';
 Write-Host Starting build
 
-Write-Host Updating base images
-docker pull microsoft/windowsservercore
-docker pull microsoft/nanoserver
-
-Write-Host Removing old images
-$ErrorActionPreference = 'SilentlyContinue';
-docker rmi $(docker images --no-trunc --format '{{.Repository}}:{{.Tag}}' | sls -notmatch -pattern '(REPOSITORY|microsoft\/(windowsservercore|nanoserver))')
-$ErrorActionPreference = 'Stop';
-Write-Host Prune system
-docker system prune -f
-
-docker build -t visualizer -f Dockerfile.windows .
+docker build --pull -t visualizer -f Dockerfile.windows .
 
 docker images
